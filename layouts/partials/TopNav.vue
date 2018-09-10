5984f0e250a792be4f28adfe03deb694b47159da
Bophelo <mpilo@vivaldi.net>
2018-09-11 01:38:45 +0200

    Initial Commit + BulmaCSS + TopNav Component

---
+++ layouts/partials/TopNav.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="navbar is-light">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+            </a>
+            <div class="navbar-burger burger">
+            <span></span>
+            <span></span>
+            <span></span>
+            </div>
+        </div>
+
+        <div  class="navbar-menu">
+            <div class="navbar-end">
+            <a class="navbar-item">
+                Sign-Up
+            </a>
+            <a class="navbar-item">
+                Sign-In
+            </a>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                Mpilo Makae
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                <a class="navbar-item">
+                    Logout
+                </a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </nav>
+</template>
